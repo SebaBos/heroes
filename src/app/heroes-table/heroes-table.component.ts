f3908b782d2bdc105c803dee7d745feb78a09f51
Sebastian <sebastian.bosiak@radmas.com>
2023-03-13 10:49:31 +0100

    initial commit

---
+++ src/app/heroes-table/heroes-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-table',
+  templateUrl: './heroes-table.component.html',
+  styleUrls: ['./heroes-table.component.css']
+})
+export class HeroesTableComponent {
+
+}
